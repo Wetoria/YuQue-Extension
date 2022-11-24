3bb3fc3e5053dba0ec1e8a2a66a014c2fc9f5e3b
Vip <vwetoria@gmail.com>
2022-11-24 10:52:23 +0800

    修复退出全屏跳转回到顶部的问题

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,77 @@
+function initMessage() {
+  const messageDom = document.createElement('span')
+  const prefix = '【极简化前端-语雀插件】'
+
+  messageDom.innerHTML = prefix + '视频控制功能加载成功'
+  messageDom.style = `
+    position: fixed;
+    background: #16AF58;
+    padding: 10px 20px;
+    z-index: 999;
+    transition: all 0.5s;
+    border: 1px solid #16AF58;
+    color: white;
+    border-radius: 10px;
+    font-weight: 500;
+  `
+  const topValue = '-44px'
+  const rightValue = 'calc(50% - 77px)'
+
+  messageDom.style.top = topValue
+  messageDom.style.right = rightValue
+
+  document.body.append(messageDom)
+  const Message = {
+    alert() {
+      setTimeout(() => {
+        // messageDom.style.right = '20px'
+        messageDom.style.top = '20px'
+  
+        setTimeout(() => {
+          // messageDom.style.right = rightValue
+          messageDom.style.top = topValue
+        }, 3000)
+      })
+    },
+    setContent(msg) {
+      msg = prefix + (msg || '视频控制功能加载成功')
+      messageDom.innerHTML = msg
+    },
+    notify(msg) {
+      this.setContent(msg)
+      this.alert()
+    }
+  }
+  return Message
+}
+const Message = initMessage()
+
+
 let a = document.querySelector('html')
-let fullscreenFlag = false
-let oldScrollTop
+let isFullscreen = false
+let oldScrollTop = 0
+let recordScrollTop = () => {
+  oldScrollTop = a.scrollTop
+}
+const addFullscreenBtnListener = () => {
+  const btns = document.querySelectorAll('[class*=Fullscreen-module_button]')
+  btns.forEach((item) => {
+    item.onclick = () => {
+      recordScrollTop()
+    }
+  })
+}
 document.onfullscreenchange = () => {
-  fullscreenFlag = !fullscreenFlag
-  if (fullscreenFlag) {
-    oldScrollTop = a.scrollTop
+  isFullscreen = !isFullscreen
+  const doSthWhenOpen = () => {
+  }
+  const doSomethingWhenClose = () => {
+      a.scrollTop = oldScrollTop
+  }
+  if (isFullscreen) {
+    doSthWhenOpen()
   } else {
-    a.scrollTop = oldScrollTop
+    doSomethingWhenClose()
   }
 }
 
@@ -108,46 +173,19 @@ document.onkeydown = (event) => {
   }
 }
 
+const changeTocAreaWidth = () => {
+  getStorage('catalogAreaWidth', (value) => {
+    const tocArea = document.querySelector('.ne-toc-normal-view')
+    if (tocArea) {
+      tocArea.style.width = value + 'px'
+    }
+  })
+}
+
+
+
 window.onload = () => {
-    const messageDom = document.createElement('span')
-    messageDom.innerHTML = '【语雀插件】视频控制功能加载成功'
-    messageDom.style = `
-      position: fixed;
-      background: #16AF58;
-      padding: 10px 20px;
-      z-index: 999;
-      transition: all 0.5s;
-      border: 1px solid #16AF58;
-      color: white;
-      border-radius: 10px;
-      font-weight: 500;
-    `
-
-    // const topValue = '20px'
-    // const rightValue = '-154px'
-
-    const topValue = '-44px'
-    const rightValue = 'calc(50% - 77px)'
-
-    messageDom.style.top = topValue
-    messageDom.style.right = rightValue
-
-    document.body.append(messageDom)
-
-    getStorage('catalogAreaWidth', (value) => {
-      const tocArea = document.querySelector('.ne-toc-normal-view')
-      if (tocArea) {
-        tocArea.style.width = value + 'px'
-      }
-    })
-
-    setTimeout(() => {
-      // messageDom.style.right = '20px'
-      messageDom.style.top = '20px'
-
-      setTimeout(() => {
-        // messageDom.style.right = rightValue
-        messageDom.style.top = topValue
-      }, 3000)
-    })
+  Message.notify()
+  changeTocAreaWidth()
+  addFullscreenBtnListener()
 }
