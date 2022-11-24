a2e263c87d168211af68cc87fe49f1ed8c67d241
Vip <vwetoria@gmail.com>
2022-11-24 12:40:57 +0800

    修复初次安装数据初始化问题

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
 let defaultOutlineWidth = 280;
-let outlineWidth = defaultOutlineWidth;
+let catalogAreaWidth = defaultOutlineWidth;
+
+let forwardStep = 1
+let backwardStep = 1
+let speedupStep = 0.1
+let speedcutStep = 0.1
+
+const defaultValues = {
+  catalogAreaWidth,
+  forwardStep,
+  backwardStep,
+  speedupStep,
+  speedcutStep,
+}
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ outlineWidth });
+  chrome.storage.sync.set(defaultValues);
   console.log('Default outlineWidth  set to ', outlineWidth);
 });
